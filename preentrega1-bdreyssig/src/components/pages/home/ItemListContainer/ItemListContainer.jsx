f92ab6d1053b2cc3d43910f4533c65c3adabe804
mariadrey17 <belydrey@yahoo.com.ar>
2023-06-02 11:27:55 -0300

    tratando de que funcionen todos los botones

--- preentrega1-bdreyssig/src/components/pages/home/ItemListContainer/ItemListContainer.jsx
+++ preentrega1-bdreyssig/src/components/pages/home/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import ItemCount from "./ItemCount/ItemCount";
+import StockDeFilms from "./StockDeFilms";
 
 
 function ItemListContainer (props) {
+
+    const onAdd= (quantity)=>{
+    console.log('comprastes ${quantity} unidades');
+    }
 return (
     <div>
         <h2>Hola {props.text}  </h2>
+
+
+    <ItemCount initial ={0} StockDeFilms={6} onAdd={onAdd}/>
     </div>
 
 );
@@ -12,3 +21,5 @@ return (
 }
 
 export default ItemListContainer;
+
+
