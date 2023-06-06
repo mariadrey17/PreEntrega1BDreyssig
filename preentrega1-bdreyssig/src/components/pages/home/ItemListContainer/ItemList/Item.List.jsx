cc7cff542017fc574da862c3626b7a16a87e1f24
mariadrey17 <belydrey@yahoo.com.ar>
2023-06-06 12:37:47 -0300

    funciona de vuelta el item count

--- preentrega1-bdreyssig/src/components/pages/home/ItemListContainer/ItemList/Item.List.jsx
+++ preentrega1-bdreyssig/src/components/pages/home/ItemListContainer/ItemList/Item.List.jsx
@@ -1,70 +1,15 @@
 import React, { useEffect, useState } from "react";
-import ItemCount from "../../ItemCount";
-import { StockDeFilms } from "./ItemCount/StockDeFilm";
-import stockfilms from "./ItemCount/StockDeFilms/stockfilms.json";
 
 
-function ItemListContainer (props) {
+function ItemList () {
 
-    /*const onAdd= (quantity)=>{
-    console.log('cantidad agregada' .quantity);
-    }*/
-
-    const resultadoSeleccion=(peliculaSeleccionada)=>{
-        return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            peliculaSeleccionada===true?resolve ("hay stock"):reject ("no hay stock");
-        }
-        
-        )
-        })
-        }
-        
-        const valor= true;
-        console.log (resultadoSeleccion(valor));
             
 
 return (
-    <div>
-        <h2>Hola {props.text}  </h2>
-
-
-    <ItemCount initial ={0} StockDeFilms={6} onAdd={(counter=> console.log('cantidad agregada',counter))}/>
-    
-    <div>
-        
-        {
-stockfilms.map ((film,prod)=>  {
-        
-        return (
-    <div key={prod}>
-        <p>{film.id}</p>
-        <p>{film.nombre}</p>
-        <p>{film.genero}</p>
-        <p>{film.precio}</p>
-    </div>
-
-
-
-
-        )
-        },
-        console.log (stockfilms)
-        )
-        }
-
-    
-    </div>
-    
-    
-
-    
-    
-    
-    </div>
+<div></div>
 
 );
 
 }
 
-export default ItemListContainer;
+export default ItemList;
