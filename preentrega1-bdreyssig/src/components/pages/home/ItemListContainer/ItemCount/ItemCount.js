e34617ea174fb3b6fb853b4cd6c1eaf2b1d80d89
mariadrey17 <belydrey@yahoo.com.ar>
2023-06-06 10:10:34 -0300

    habia dos list container jsx borre 1

---
+++ preentrega1-bdreyssig/src/components/pages/home/ItemListContainer/ItemCount/ItemCount.js
@@ -0,0 +1,52 @@
+//creando estructura del itemcount
+
+import React from "react";
+//import "./ItemCount.css";
+import {useState} from "react";
+import StockDeFilms from "../StockDeFilms/StockDeFilms";
+
+export const ItemCount=({initial,StockDeFilms,onAdd})=>{
+    const [count,setCount]=useState(initial);
+    const decrease=()=>{
+        setCount(count -1);
+    }
+
+    const increase=()=>{
+        setCount(count+1);
+    }
+    return (
+        <div>
+            <h3>Carrito</h3>
+            <div className="counter">
+
+
+
+    
+            <button disabled={count<= 0} onClick={decrease}>-</button>
+        
+        <button >
+        <span>{count}</span>
+        </button>
+
+
+            <button disabled={count>=StockDeFilms} onClick={increase}>+</button>
+        
+
+            <div>
+                <button disabled={StockDeFilms<= 0} onClick={()=>onAdd(count)}>Agregar carrito </button>
+            </div>
+
+
+            </div>
+    
+    
+
+
+
+        </div>
+
+    
+    );
+}
+
+export default ItemCount ;
