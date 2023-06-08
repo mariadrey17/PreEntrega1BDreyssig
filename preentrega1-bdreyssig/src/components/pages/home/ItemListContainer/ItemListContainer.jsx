d4395d88362fc3eb979c136e7c65773549914445
mariadrey17 <belydrey@yahoo.com.ar>
2023-06-08 17:29:46 -0300

    resolviendo los errores que obviamente tira los cambios y pasando todo para tambien resolverlos

---
+++ preentrega1-bdreyssig/src/components/pages/home/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,72 @@
+//import React, { useEffect, useState } from "react";
+import ItemCount from "../ItemCount";
+import stockrecomendados from "../StockDeRecomendados/stockrecomendados";
+
+
+function ItemListContainer (props) {
+
+    /*const onAdd= (quantity)=>{
+    console.log('cantidad agregada' .quantity);
+    }*/
+
+    const resultadoSeleccion=(peliculaSeleccionada)=>{
+        return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            peliculaSeleccionada===true?resolve ("hay stock"):reject ("no hay stock");
+        }
+        
+        )
+        })
+        }
+        
+        const valor= true;
+        console.log (resultadoSeleccion(valor));
+            
+
+
+return (
+    <div>
+        <h2>Hola {props.text}  </h2>
+
+
+    <ItemCount initial ={0} stockrecomendados={6} onAdd={(counter=> console.log('cantidad agregada',counter))}/>
+    
+    <div>
+        
+        {
+stockrecomendados.map ((film,prod)=>  {
+        
+        return (
+    <div key={prod}>
+        <p>{film.id}</p>
+        <p>{film.nombre}</p>
+        <p>{film.genero}</p>
+        <p>{film.precio}</p>
+        {/*<img>{film.image}</p>*/}
+        <img src={film.image} alt="" />
+    </div>
+
+
+
+
+        )
+        },
+        console.log (stockrecomendados)
+        )
+        }
+
+    
+    </div>
+    
+    
+
+    
+    
+    
+    </div>
+
+);
+
+}
+
+export default ItemListContainer;
