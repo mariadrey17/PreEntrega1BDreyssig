265d39c3e8e171ae164acc650046da7050c1f33f
mariadrey17 <belydrey@yahoo.com.ar>
2023-06-09 03:50:48 -0300

    ya va tomando forma el stock de recomendados con los maps

--- preentrega1-bdreyssig/src/components/pages/home/ItemListContainer/ItemListContainer.jsx
+++ preentrega1-bdreyssig/src/components/pages/home/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,7 @@
 //import React, { useEffect, useState } from "react";
 import ItemCount from "../ItemDetailContainer/ItemCount/ItemCount";
-import stockrecomendados from "../StockRecomendados/stockrecomendados";
+import  stockrecomendados from "../StockRecomendados/stockrecomendados";
+import ItemList from "../ItemList/ItemList"
 
 
 function ItemListContainer (props) {
@@ -30,33 +31,8 @@ return (
 
 
     <ItemCount initial ={0} stockrecomendados={6} onAdd={(counter=> console.log('cantidad agregada',counter))}/>
-    <div>
-        
-        {
-stockrecomendados.map ((film,prod)=>  {
-        
-        return (
-    <div key={prod}>
-        <p>{film.id}</p>
-        <p>{film.nombre}</p>
-        <p>{film.genero}</p>
-        <p>{film.precio}</p>
-        {/*<img>{film.image}</p>*/}
-        <img src={film.image} alt="" />
-    </div>
 
-
-
-
-        )
-        },
-        console.log (stockrecomendados)
-        )
-        }
-
-    
-    </div>
-    
+    <ItemList/>
     
 
     
