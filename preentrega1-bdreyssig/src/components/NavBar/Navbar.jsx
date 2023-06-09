80a6809ff1780fcdcde593f1e59b24ea4505103f
mariadrey17 <belydrey@yahoo.com.ar>
2023-06-09 04:01:34 -0300

    pasando el nuevo navbar con los enlaces activos

--- preentrega1-bdreyssig/src/components/NavBar/Navbar.jsx
+++ preentrega1-bdreyssig/src/components/NavBar/Navbar.jsx
@@ -1,40 +1,58 @@
 
-    import Container from 'react-bootstrap/Container';
-    import Nav from 'react-bootstrap/Nav';
-    import Navbar from 'react-bootstrap/Navbar';
-    import NavDropdown from 'react-bootstrap/NavDropdown';
-    import CartWidget from './CartWidget/CartWidget';
-    import './nav.css';
-    import logo from '../../Assets/img/logo.jpg';
-    import ItemCount from '../pages/home/ItemDetailContainer/ItemCount/ItemCount';
-    
-    function NavBar() {
-    return (
-        <Navbar  expand="lg" className='nav-style' >
-        <Container >
-        <img src={logo}  className='logo' alt="" /><h1>VisualFilm Shop</h1>
-    
-        
-            <Navbar.Brand href="#home">Las Mejores Películas y Series </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Peliculas</Nav.Link>
-                <NavDropdown title="Catálogo" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Series</NavDropdown.Item>
-                
-                <NavDropdown.Item href="#action/3.3">Contacto</NavDropdown.Item>
-                
-                </NavDropdown>
-            </Nav>
-            </Navbar.Collapse>
-            <CartWidget></CartWidget>
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import CartWidget from './CartWidget/CartWidget';
+import './nav.css';
+import logo from '../../Assets/img/logo.jpg';
+import ItemCount from '../pages/home/ItemDetailContainer/ItemCount/ItemCount';
+import {Link, NavLink} from 'react-router-dom';
 
-            
-        </Container>
-        </Navbar>
-    );
-    }
+
+function NavBar() {
+return (
+    <Navbar  expand="lg" className='nav-style' >
+    <Container >
+<Link to='/' > <img src={logo} className='logo' alt="" /></Link><h1>VisualFilm Shop</h1> 
+
     
-    export default NavBar ;
+        <NavLink to="/">home        Las Mejores Películas y Series </NavLink>
+
+        <ul className='navstyle'>
+        <li>
+            <NavLink to= "/peliculas">Películas y Series</NavLink>
+
+            </li>
+        <li> <NavLink to= "/series">Series</NavLink></li>
+        <li>
+            <NavLink  to="/contacto" >Contacto</NavLink> </li>
+
+        </ul>
+
+
+        {/*<Navbar.Toggle aria-controls to="basic-navbar-nav" />
+        <Navbar.Collapse id to="basic-navbar-nav">
+        <Nav className="me-auto">
+            <Nav.Link to ="/">Home</Nav.Link>
+            <Nav.Link to="/">Peliculas</Nav.Link>
+            <NavDropdown title  ="Catálogo" id="basic-nav-dropdown">
+            <NavDropdown.Item to="/series">Series</NavDropdown.Item>
+            
+            <NavDropdown.Item to ="7contacto">Contacto</NavDropdown.Item>
+            
+            </NavDropdown>
+        </Nav>
+</Navbar.Collapse>*/}
+        <CartWidget></CartWidget>
+    
+
+
+
+    </Container>
+    </Navbar>
+);
+}
+
+export default NavBar ;
+
--- preentrega1-bdreyssig/src/components/pages/home/Home.jsx
+++ preentrega1-bdreyssig/src/components/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 
 import Estrenos from "./Estrenos/Estrenos";
+import Link from "react-router-dom";
 
 
 import Form from "../../Layouts/Formularios/Formulario";
