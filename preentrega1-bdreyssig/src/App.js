967491625eee9c69a3cdb373bc3794c0c5203bd0
mariadrey17 <belydrey@yahoo.com.ar>
2023-06-10 13:54:30 -0300

    aca cree el itemdetail en el proyecto original

--- preentrega1-bdreyssig/src/App.js
+++ preentrega1-bdreyssig/src/App.js
@@ -1,66 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar/Navbar';
-import CartWidget from './components/NavBar/CartWidget/CartWidget';
-import { Button } from 'bootstrap';
-import { BiCartAdd } from "react-icons/bi" ; 
-import Home from './components/pages/home/Home';
-import Estrenos from './components/pages/home/Estrenos/Estrenos';
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar/Navbar";
+import CartWidget from "./components/NavBar/CartWidget/CartWidget";
+import { Button } from "bootstrap";
+import { BiCartAdd } from "react-icons/bi";
+import Home from "./components/pages/home/Home";
+import Estrenos from "./components/pages/home/Estrenos/Estrenos";
 
-import Peliculas from './components/pages/Peliculas/peliculas';
-import './App.css'
-import Series from './components/pages/Series/Series';
-import {BrowserRouter,Route,Routes,NavLink,Link} from "react-router-dom";
-import IniciarSesion from './components/Layouts/Formularios/IniciarSesion/IniciarSesion';
-import Footer  from './components/pages/Footer/Footer';
-import Contacto from './components/pages/Contacto/Contacto';
-import ItemListContainer from './components/pages/home/ItemListContainer/ItemListContainer';
+import Peliculas from "./components/pages/Peliculas/peliculas";
+import "./App.css";
+import Series from "./components/pages/Series/Series";
+import { BrowserRouter, Route, Routes, NavLink, Link } from "react-router-dom";
+import IniciarSesion from "./components/Layouts/Formularios/IniciarSesion/IniciarSesion";
+import Footer from "./components/pages/Footer/Footer";
+import Contacto from "./components/pages/Contacto/Contacto";
+import ItemListContainer from "./components/pages/home/ItemListContainer/ItemListContainer";
 //import ItemList from './components/pages/home/ItemListContainer/ItemList/Item.List';
 
-
-
-
 function App() {
-    return (
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Footer />
 
-<BrowserRouter>
-<NavBar/>
-<Footer/>
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        {/* <Route path="/" element={<Home />} />*/}
+        <Route path="/home" element={<Home />} />
 
+        <Route path="/peliculas" element={<Peliculas />} />
+        {/*<Route exact path ="/"element={<ItemList/>} />*/}
 
+        <Route path="/series" element={<Series />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/footer" element={<Footer />} />
+      </Routes>
+    </BrowserRouter>
 
-<Routes>
-
-<Route path="/" element={<Home/>}/>
-<Route path="/home/id:4" element={<Home/>}/>
-
-<Route path="/" element={<ItemListContainer/>}/>
-<Route path="/peliculas"element={<Peliculas/>} />
-{/*<Route exact path ="/"element={<ItemList/>} />*/}
-
-<Route  path="/series" element= {<Series/>} />
-<Route  path="/contacto" element= {<Contacto/>} />
-<Route  path="/footer" element= {<Footer/>} />
-
-
-
-
-
-
-
-
-
-</Routes>
-
-
-
-
-
-
-</BrowserRouter>
-
-/*<NavBar></NavBar>
+    /*<NavBar></NavBar>
 
 <Home>
 
@@ -92,16 +70,7 @@ function App() {
 
 
 <Footer/>*/
-
-
-
-
-
-
-    );
-    
-    }
+  );
+}
 
 export default App;
-
-
