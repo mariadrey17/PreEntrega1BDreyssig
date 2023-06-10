bea213c5ea2f1237dc6b9a42be59a1f3a5fdab21
mariadrey17 <belydrey@yahoo.com.ar>
2023-06-10 11:37:16 -0300

    trtando de que aparezcan las cards

--- preentrega1-bdreyssig/src/components/pages/home/ItemListContainer/ItemListContainer.jsx
+++ preentrega1-bdreyssig/src/components/pages/home/ItemListContainer/ItemListContainer.jsx
@@ -3,32 +3,69 @@ import { useParams } from "react-router-dom";
 //import ItemCount from "../ItemCount";
 import stockrecomendados from "../../../../data/stockrecomendados";
 import { useState,useEffect } from "react";
-import {  getProductFilms } from "../../../../data/async";
+import { getProductFilm, getProductFilms } from "../../../../data/async";
 //import ItemList from "./ItemList/Item.List";
-
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import Home from "../Home";
 
 export const ItemListContainer=()=>{
-    const {id}=useParams();
-    const [stockrecomendados,setStockrecomendados]=useState([]);
-    useEffect(()=>{
-    getProductFilms().then((data)=>{
-    setStockrecomendados(data);
-    })
-    },[]
-    
-    );
+
+const {id}=useParams();
+const [stockrecomendados,setStockrecomendados]=useState([]);
+useEffect(()=>{
+getProductFilms().then((data)=>{
+setStockrecomendados(data);
+})
+},[]
+
+);
 
 //return <div>{JSON.stringify(stockrecomendados)}</div>
 
 
-return (
-<div>
+{stockrecomendados.map ((film,prod)=>  {
+        
+    return (
+
+<>
 <div>
     <Home/>
 </div>
 
 
+<div className="divCardMap"   key={prod} >
+
+
+
+{/*<Card style={{ width: '10rem' }}>
+<Card.Img variant="top" src=  {film.image} />
+<Card.Body>
+<Card.Text>{film.id}</Card.Text>
+<Card.Title>{film.nombre}</Card.Title>
+<Card.Text>{film.genero}</Card.Text>
+<Card.Text>{film.precio}</Card.Text>
+
+    </Card.Body>*/}
+
+
+
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title> {film.nombre}</Card.Title>
+        <Card.Text>
+          {film.id}
+        </Card.Text>
+          
+        <Card.Text>
+        {film.genero}
+          </Card.Text>
+          
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
 
 
 
@@ -36,12 +73,21 @@ return (
 
 
 
+
+{/*</Card>*/}
 
 </div>
 
-)
+</>
+
+
+    );
+    },
+    //console.log (stockrecomendados)
+    );
+    }
+        
+
 
 }
-
-
 export default ItemListContainer;
