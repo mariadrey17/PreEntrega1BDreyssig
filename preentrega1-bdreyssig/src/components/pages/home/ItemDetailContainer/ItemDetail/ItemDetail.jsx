65fdd15862d10c48a2e9c677c88ed88dfe7c9fef
mariadrey17 <belydrey@yahoo.com.ar>
2023-06-11 03:48:20 -0300

    agregando y ajustando nuevo codigo de itemlist container  e itemdetaail container

--- preentrega1-bdreyssig/src/components/pages/home/ItemDetailContainer/ItemDetail/ItemDetail.jsx
+++ preentrega1-bdreyssig/src/components/pages/home/ItemDetailContainer/ItemDetail/ItemDetail.jsx
@@ -1,10 +1,54 @@
-import { useParams } from 'react-router-dom';
-import stockrecomendados from "../../../../../data/stockrecomendados";
-import Card from 'react-bootstrap/Card';
+import { useParams } from "react-router-dom";
+import stockrecomendados from "../../StockDeFilms/stockfilms.json";
+import Item from "../../../Comp2/Item";
+import ItemCount from "../ItemCount";
 
-const ItemDetail =()=>({id,nombre, descripcion, categoryId}){
+const ItemDetail = () => {
+  const { filmId } = useParams();
+  const film = stockrecomendados.find((film) => film.id === filmId);
+  
+  return (
+    <div className="SectionFilm">
+      <p> Este es el detalle del film </p>
 
-    return(
-        <div></div>
-    )
-}
+      <div>
+        <Item
+          id={"04"}
+          nombre={"Aquaaman"}
+          genero={"Aventura"}
+          precio={"2000"}
+          categoryId={"peliculas"}
+        />
+      </div>
+      {/*<div className="CardBody">
+        <ul>
+          {id}
+          {nombre} {genero}
+          {precio} {categoryId}
+  </ul>
+        <div className="CardDiv">
+          <h2 className="CardTitle"> {nombre}</h2>
+        </div>
+
+        <div>
+          <p>{genero}</p>
+        </div>
+
+        <div>
+          <p>{price}</p>
+  </div>
+      </div>*/}
+
+      <div>
+        <ItemCount
+          initial={0}
+      stockrecomendados={6}
+          onAdd={(counter) => console.log("cantidad agregada", counter)}
+        />
+      </div>
+    </div>
+  );
+};
+export default ItemDetail;
+
+
