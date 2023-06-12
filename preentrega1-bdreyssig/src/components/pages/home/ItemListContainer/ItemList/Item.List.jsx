24baa2f284efe50fa84f2bdaa864b4f54039deaf
mariadrey17 <belydrey@yahoo.com.ar>
2023-06-12 16:25:00 -0300

    pasando item e itemlist a la entrega original

--- preentrega1-bdreyssig/src/components/pages/home/ItemListContainer/ItemList/Item.List.jsx
+++ preentrega1-bdreyssig/src/components/pages/home/ItemListContainer/ItemList/Item.List.jsx
@@ -3,40 +3,24 @@ import React from "react";
 import stockrecomendados from "../../StockRecomendados/stockrecomendados";
 
 import Card from 'react-bootstrap/Card';
-
-import Item from "../../Item/Item";
+import "./ItemList.css";
+import Item from "./Item/Item";
 
 
 const ItemList =({data=[]})=>{
     return (
-   // data.map(stockrecomendados=><Item key={stockrecomendados.id} info={stockrecomendados}/>)
+    data.map(stockrecomendados=><Item key={stockrecomendados.id} info={stockrecomendados}/>)
 
 
 
-    <div >
 
-        <h3>Top recomendado</h3>
+
+      
+   
         
-        {stockrecomendados.map ((film,prod)=>  {
-        
-    <div className="divCardMap"   key={prod} >
-
-
-<Item name={film.nombre} image={film.image } genero ={film.genero} precio={film.precio} id={film.id}/>
-
-
-    </div>
-
-
-
-        
-        },
-        //console.log (stockrecomendados)
-        )
-        }
 
     
-    </div>
+   
 
 
 
@@ -57,3 +41,20 @@ const ItemList =({data=[]})=>{
     );
 }
 export default ItemList;
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+    
+
+
+
