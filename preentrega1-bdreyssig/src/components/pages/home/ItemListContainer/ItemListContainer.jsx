fac0c2471f2c8516ff19ef8cb746d40a09694399
mariadrey17 <belydrey@yahoo.com.ar>
2023-06-12 16:32:21 -0300

    y pse el list container

--- preentrega1-bdreyssig/src/components/pages/home/ItemListContainer/ItemListContainer.jsx
+++ preentrega1-bdreyssig/src/components/pages/home/ItemListContainer/ItemListContainer.jsx
@@ -1,21 +1,34 @@
 import React, { useEffect, useState } from "react";
 import ItemCount from "../ItemDetailContainer/ItemCount/ItemCount"
-import stockrecomendados  from "../StockRecomendados/stockrecomendados";
+import stockrecomendados from "../StockRecomendados/stockrecomendados"
 import ItemList from "./ItemList/Item.List";
-import { getProductFilms } from "../../../../../src/datos/asyncses/async";
+
+import { useParams } from "react-router-dom";
 
 
 function ItemListContainer (props) {
 
+const [data,setData]=useState([]);
+
+const{id}=useParams
+
+useEffect(()=>{
+const getData =new Promise(resolve=>setTimeout(()=>{
+    resolve(stockrecomendados)
+},2000));
+getData.then(res=>setData(res)  )
+},[])
+
+
 
     
-    const [stockrecomendados,setStockrecomendados]=useState([]);
+    /*const [stockrecomendados,setStockrecomendados]=useState([]);
     useEffect (()=>{
         getProductFilms().then(response=>{
             setStockrecomendados(response)
         })
         .catch(error=>console.error(error))
-    },[])
+    },[])*/
 
  
 
@@ -25,12 +38,17 @@ return (
     <div>
         <h2>Hola {props.text}  </h2>
 
-    
+
     
 
 
     <ItemCount initial ={0} stockrecomendados={6} onAdd={(counter=> console.log('cantidad agregada',counter))}/>
-    <ItemList />
+
+
+
+    <h3>Top de recomendados</h3>
+
+    <ItemList data={data} />
     
 
     
