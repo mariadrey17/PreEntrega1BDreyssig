3f5da20739b369ff2072a9449345cc7cde49e406
mariadrey17 <belydrey@yahoo.com.ar>
2023-06-27 10:29:06 -0300

    agregue la carpeta datos a el main

---
+++ preentrega1-bdreyssig/src/datos/peliculas.js
@@ -0,0 +1,88 @@
+const films =
+  //array de stock de films
+  [
+    {
+      id: 1,
+      nombre: "film1",
+      titulo: "Start strek ,contacto",
+      categoryId: "peliculas",
+      genero: "ciencia ficcion",
+      descripcion:
+        "Picard, Riker y otros se preparan para evitar que el medio-robot Borg sabotee el histórico vuelo de un cohete en el año 2063.",
+      precio: "$800",
+      image:
+        "https://es.web.img2.acsta.net/c_310_420/medias/nmedia/18/79/66/86/20136104.jpg",
+    },
+    {
+      id: 2,
+      nombre: "film2",
+      titulo: "Lo que hacemos en las sombras ",
+      categoryId: "estrenos",
+      genero: "comedia",
+      descripcion:
+        "Picard, Riker y otros se preparan para evitar que el medio-robot Borg sabotee el histórico vuelo de un cohete en el año 2063.",
+      precio: "$800",
+      image:
+        "https://pics.filmaffinity.com/Lo_que_hacemos_en_las_sombras-587601603-large.jpg",
+    },
+    {
+      id: 3,
+      nombre: "El padrino 3",
+      categoryId: "peliculas",
+      genero: "drama",
+      descripcion:
+        "Picard, Riker y otros se preparan para evitar que el medio-robot Borg sabotee el histórico vuelo de un cohete en el año 2063.",
+      precio: "$950",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgEy91ccbAzTtI-TnRSiIe2aGO440xH0o6rg&usqp=CAU",
+    },
+    {
+      id: 4,
+      nombre: " Aquaman 2",
+      categoryId: "estrenos",
+      genero: "ciencia ficcion, aventura",
+      precio: "$2000",
+      image:
+        "https://i.blogs.es/36305a/captura-de-pantalla-2022-09-06-a-las-13.08.10/450_1000.jpeg",
+    },
+    {
+      id: 5,
+      nombre: "fenómenas",
+      categoryId: "peliculas",
+      genero: "comedia , terror",
+      precio: "$2800",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmOHc8DQFmrxjuUOiivwBMINmWwEOyamrl_5uu7BT63_RZyliA7Ii0UUWMGXp1rqgYvsE&usqp=CAU",
+    },
+    {
+      id: 6,
+      nombre: "Start strek ,contacto",
+      categoryId: "series",
+      genero: "ciencia ficcion",
+      precio: "$2000",
+      image:
+        "https://es.web.img2.acsta.net/c_310_420/medias/nmedia/18/79/66/86/20136104.jpg",
+    },
+  ];
+
+export default films;
+
+export const getFilms = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(films);
+    }, 2000);
+  });
+};
+export const getFilmsById = (id) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(films.find((film) => film.id === parseInt(id)));
+    }, 2000);
+  });
+};
+export const getFilmsByCategory = async (categoryId) => {
+  return new Promise((resolve, reject) => {
+    resolve(films.filter((category) => category.categoryId === categoryId));
+  });
+};
