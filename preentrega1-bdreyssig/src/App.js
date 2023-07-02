c88eafe4e5b6ff79703106f5dff569b2a6263b13
mariadrey17 <belydrey@yahoo.com.ar>
2023-07-02 13:37:04 -0300

    firebase afecto mi proyecto

--- preentrega1-bdreyssig/src/App.js
+++ preentrega1-bdreyssig/src/App.js
@@ -12,6 +12,7 @@ import Peliculas from "./components/pages/Peliculas/peliculas";
 import "./App.css";
 import Series from "./components/pages/Series/Series";
 import { BrowserRouter, Route, Routes, NavLink, Link } from "react-router-dom";
+
 import IniciarSesion from "./components/Layouts/Formularios/IniciarSesion/IniciarSesion";
 import Footer from "./components/pages/Footer/Footer";
 import Contacto from "./components/pages/Contacto/Contacto";
