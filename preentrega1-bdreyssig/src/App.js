f2477953373fa9056ce9fa565d7a0377dd9e362a
mariadrey17 <belydrey@yahoo.com.ar>
2023-07-02 13:55:37 -0300

    acabo de psar el sdk en app js en la rama espero que funcione todo lo que haga aca y cree carpeta services

--- preentrega1-bdreyssig/src/App.js
+++ preentrega1-bdreyssig/src/App.js
@@ -15,6 +15,28 @@ import Home from "./components/pages/home/Home";
 import CartWidget from "./components/NavBar/CartWidget/CartWidget";
 import CartProvider from "./components/context/CartContext";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD5hnX1fLdveVNEjqrnkCOo_CIGNIZBqRo",
+  authDomain: "proyectofinal-5e6de.firebaseapp.com",
+  projectId: "proyectofinal-5e6de",
+  storageBucket: "proyectofinal-5e6de.appspot.com",
+  messagingSenderId: "375425842853",
+  appId: "1:375425842853:web:59918b4a864015bba6174c",
+  measurementId: "G-LE2Q9LCVKL",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <CartProvider>
