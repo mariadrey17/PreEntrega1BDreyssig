62257e8514c9aa50425ac6f3ce25d2eed5ce4c2c
mariadrey17 <belydrey@yahoo.com.ar>
2023-07-02 01:11:34 -0300

    trabaje el código experimentando haciendo pruebas con el cart, cart context y todo el carrito

--- preentrega1-bdreyssig/src/components/Cart/Cart.js
+++ preentrega1-bdreyssig/src/components/Cart/Cart.js
@@ -1,25 +1,45 @@
-import { useContext } from "react";
-import { CartContext } from "../context/CartContext";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { CartContext } from "./CartContext";
 
 const Cart = () => {
-  const {
-    cart,
-    addItem,
-    removeItem,
-    clear,
-    quantity,
-    totalCart,
-    someItemsCart,
-  } = useContext(CartContext);
+  const { cart, addItem, removeItem, clear, someItemsCart, totalCart } =
+    useContext(CartContext);
 
-  if (totalCart === 0) {
+  if (cart.length === 0) {
     return (
       <div>
-        <h1> No hay Items en el carrito</h1>
+        <h1>No hay Items en el carrito</h1>
         <Link to="/" className="option">
           Top Recomendados
         </Link>
       </div>
     );
   }
+
+  return (
+    <div>
+      <h2>Cart</h2>
+      {cart.length === 0 ? (
+        <p>Tu carrito está vacío</p>
+      ) : (
+        <div>
+          <ul>
+            {cart.map((item) => (
+              <li key={item.id}>
+                {item.title} - quantity: {item.cantidad}
+                <button onClick={() => removeItem(item.id)}>eliminar</button>
+              </li>
+            ))}
+          </ul>
+          <Link to="/checkout" className="option">
+            Checkout
+          </Link>
+          <p>Total: {totalCart()}</p>
+        </div>
+      )}
+    </div>
+  );
 };
+
+export default Cart;
--- preentrega1-bdreyssig/src/components/NavBar/CartWidget/CartWidget.jsx
+++ preentrega1-bdreyssig/src/components/NavBar/CartWidget/CartWidget.jsx
@@ -1,29 +1,68 @@
-import { Button } from "bootstrap";
-import React from "react";
-import ItemCount from "../../pages/home/ItemDetailContainer/ItemCount/ItemCount";
+import React, { useContext } from "react";
+import { BiCartAdd } from "react-icons/bi";
+import { CartContext } from "../../context/CartContext";
+import { Link } from "react-router-dom";
 
+const CartWidget = () => {
+  const { totalCart, addItem, film } = useContext(CartContext);
 
-import { BiCartAdd } from "react-icons/bi" ; 
+  const handleAddToCart = () => {
+    // Aquí puedes llamar a la función addItem del contexto para agregar una película al carrito
+    const filmToAdd = {
+      id: 1, // ID de la película que deseas agregar
+      nombre: "Título de la película", // Nombre de la película que deseas agregar
+      precio: 10, // Precio de la película que deseas agregar
+      image: "imagen.jpg", // URL de la imagen de la película que deseas agregar
+      cantidad: 1, // Cantidad de la película que deseas agregar
+    };
+    addItem(filmToAdd, filmToAdd.cantidad);
+  };
 
-
-export const CartWidget =()=>{
-    const onAdd= (quantity)=>{
-        console.log('comprastes ${quantity} unidades');
-        }
-    return (
+  return (
+    <div>
+      <button onClick={handleAddToCart}>
+        <Link
+          to="/Cart"
+          className="CartWidget"
+          style={{ display: totalCart > 0 ? "block" : "none" }}
+        >
+          <BiCartAdd />
+          <span>{totalCart}</span>
+        </Link>
+        <p>Carrito: {totalCart} elementos</p>
+      </button>
+    
+      {film && (
         <div>
-    <button>
-        < BiCartAdd></BiCartAdd>
-        <p>0</p>
-    </button>
+          <h1>Película adquirida</h1>
+          <ul>
+            <li>{film.id}</li>
+            <li>{film.nombre}</li>
+            <li>{film.precio}</li>
+            <li>{film.image}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CartWidget;
+
+
+
+
+
+   /* const onAdd= (quantity)=>{
+        console.log('comprastes ${quantity} unidades');
+        }*/
+  
+       
+            {/*<Link  to = '/Cart'className="CartWidget" style ={{display:totalCart>0? 'block' :'none'}}>
+                {totalCart}
+    </Link>*/}
+
     
 {/*<ItemCount initial ={0} StockDeFilms={6} onAdd={onAdd} />*/}
 
 
-        </div>
-
-    
-    );
-}
-
-export default CartWidget ;
