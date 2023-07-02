ae46cad5b7668d9ebdae5567d55b74afe61c08c1
mariadrey17 <belydrey@yahoo.com.ar>
2023-07-02 13:53:14 -0300

    parece que soluciones el error poniendo el sdk en app y no en index

--- preentrega1-bdreyssig/src/App.js
+++ preentrega1-bdreyssig/src/App.js
@@ -21,6 +21,28 @@ import ItemDetailContainer from "./components/pages/home/ItemDetailContainer/Ite
 import ItemDetail from "./components/pages/home/ItemDetailContainer/ItemDetail/ItemDetail";
 //import ItemList from './components/pages/home/ItemListContainer/ItemList/Item.List';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD5hnX1fLdveVNEjqrnkCOo_CIGNIZBqRo",
+  authDomain: "proyectofinal-5e6de.firebaseapp.com",
+  projectId: "proyectofinal-5e6de",
+  storageBucket: "proyectofinal-5e6de.appspot.com",
+  messagingSenderId: "375425842853",
+  appId: "1:375425842853:web:59918b4a864015bba6174c",
+  measurementId: "G-LE2Q9LCVKL",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <BrowserRouter>
