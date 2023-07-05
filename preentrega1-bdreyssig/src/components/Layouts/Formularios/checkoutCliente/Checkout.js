cb654199203874edcac1fbea488d36e3adf35433
mariadrey17 <belydrey@yahoo.com.ar>
2023-07-05 19:00:49 -0300

    error en linea 18

--- preentrega1-bdreyssig/src/components/Layouts/Formularios/checkoutCliente/Checkout.js
+++ preentrega1-bdreyssig/src/components/Layouts/Formularios/checkoutCliente/Checkout.js
@@ -7,52 +7,49 @@ const firestore = getFirestore();
 const ordersCollectionRef = collection(firestore, "orden"); // Reemplazando 'orden' con el nombre de tu colección
 
 function Checkout() {
-  const [orderId, setOrderId] = useState("");
-  const enviarOrden = () => {
-    const orden = {
-      comprador: { name: "", email: "" },
-      filmName: [{ name: "skyrojo", price: 2000 }],
-      TotalCart: 2000,
-    };
-    const db = getFirestore();
-    const ordenesCollection = collection(db, "ordenes");
+  const [setOrderId] = useState("");
 
-    addDoc(ordenesCollection, orden).then((id) => setOrderId(id));
+  const db = getFirestore();
+  const ordenesCollection = collection(db, "ordenes");
+
+  addDoc(ordenesCollection, orden).then((id) => setOrderId(id));
+}
+
+const [clientName, setClientName] = useState("");
+const [email, setEmail] = useState("");
+const [filmName, setFilmName] = useState("");
+const [quantity, setQuantity] = useState("");
+const [deliveryDate, setDeliveryDate] = useState("");
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+
+  // Creando un objeto con los valores del formulario
+  const orderData = {
+    clientName,
+    email,
+    filmName,
+    quantity,
+    deliveryDate,
   };
 
-  const [clientName, setClientName] = useState("");
-  const [email, setEmail] = useState("");
-  const [filmName, setFilmName] = useState("");
-  const [quantity, setQuantity] = useState("");
-  const [deliveryDate, setDeliveryDate] = useState("");
+  try {
+    // Agregando los valores a la colección de Firestore
+    // Configurando la referencia a la colección de Firestore
+    const firestore = getFirestore();
+    const ordersCollectionRef = collection(firestore, "orden"); // Reemplazando 'orden' con el nombre de tu colección
+    const docRef = await addDoc(ordersCollectionRef, orderData);
+    console.log("Orden de compra enviada correctamente. ID:", docRef.id);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    // Creando un objeto con los valores del formulario
-    const orderData = {
-      clientName,
-      email,
-      filmName,
-      quantity,
-      deliveryDate,
-    };
-
-    try {
-      // Agregando los valores a la colección de Firestore
-      const docRef = await addDoc(ordersCollectionRef, orderData);
-      console.log("Orden de compra enviada correctamente. ID:", docRef.id);
-
-      // Reiniciando los campos del formulario después de enviar los valores
-      setClientName("");
-      setEmail("");
-      setFilmName("");
-      setQuantity("");
-      setDeliveryDate("");
-    } catch (error) {
-      console.error("Error al enviar la orden de compra:", error);
-    }
-  };
+    // Reiniciando los campos del formulario después de enviar los valores
+    setClientName("");
+    setEmail("");
+    setFilmName("");
+    setQuantity("");
+    setDeliveryDate("");
+  } catch (error) {
+    console.error("Error al enviar la orden de compra:", error);
+  }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -100,6 +97,6 @@ function Checkout() {
       <button type="submit">Enviar orden de compra</button>
     </form>
   );
-}
+};
 
 export default Checkout;
