ab47c21a0c0eee99cee6cf44b14cf2053ebcca10
mariadrey17 <belydrey@yahoo.com.ar>
2023-07-06 18:00:39 -0300

    hay cambios en el itemdetail

--- preentrega1-bdreyssig/src/components/pages/home/ItemDetailContainer/ItemDetail/ItemDetail.jsx
+++ preentrega1-bdreyssig/src/components/pages/home/ItemDetailContainer/ItemDetail/ItemDetail.jsx
@@ -1,1 +1,60 @@
-<img src= {image}/>
+import { useParams } from "react-router-dom";
+import { useContext } from "react";
+
+import ItemCount from "../ItemCount/ItemCount";
+import { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+import { CartContext } from "../../../../Context/CartContext";
+
+const ItemDetail = ({image,titulo,precio,id, genero,categoryId}) => {
+
+
+const [quantityAdded,setQuantityAdded]=useState(0)
+const {addItem}=useContext(CartContext)
+
+const handleOnAdd=(quantity)=>{
+  setQuantityAdded(quantity)
+  const film = {id,titulo ,precio}
+  addItem(film,quantity)
+  
+}
+
+    
+  return (
+    <div className="SectionFilm">
+      <p> Este es el detalle del film </p>
+
+      <ul>
+        <li>{id}</li>
+        <li>{titulo}</li>
+        <li>{genero}</li>
+        <li>{precio}</li>
+        <img src= {image}/>
+        <li>{categoryId}</li>
+</ul>
+  
+<Link to={`/cart`}>ir al carrito</Link> 
+         
+       
+        
+      <div>
+        <ItemCount initial ={0} films={6} onAdd={handleOnAdd}/>
+
+
+  </div>
+        
+    
+
+
+    </div>
+  );
+};
+export default ItemDetail;
+
+
+
+
+
+
+
