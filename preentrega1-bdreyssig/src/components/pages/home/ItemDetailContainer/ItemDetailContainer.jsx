fb306e4b22d314f84e30c8acbf3daa0c8ef87097
mariadrey17 <belydrey@yahoo.com.ar>
2023-07-06 13:49:47 -0300

    ya agregue el itemdetailcontainer

--- preentrega1-bdreyssig/src/components/pages/home/ItemDetailContainer/ItemDetailContainer.jsx
+++ preentrega1-bdreyssig/src/components/pages/home/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,82 +1,41 @@
-
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ItemDetail from "./ItemDetail/ItemDetail";
-import { getFilmsById } from "../../../../datos/peliculas";
+import { getFilmsById } from "../../../../services/firebase/firebaseConfig";
 
+const ItemDetailContainer = () => {
+  const [film, setFilm] = useState({});
+  const { id } = useParams();
 
-
-const ItemDetailContainer =()=>{
-  const [film ,setFilm]=useState({})
-  const{id}=useParams();
-useEffect(()=>{
-    getFilmsById(id)
-    .then((response)=>{
-      setFilm(response)
-    })
-.catch(error=>{
-    console.log (error)
-})
-},[id])
-
-
-
-
-/*return (
-<div className="ItemDetailContainer">
-<ItemDetail {...StockDeFilms}/>
-</div>
-);
-}
-export default ItemDetailContainer;*/
-
-/*export const ItemDetailContainer =()=>{
-
-    const [stockfilms,setStockfilms]=useState(null)
-
-    useEffect(()=>{
-    getProductFilmId =('1')
-    .then ((response)=>{setStockfilms})
-    })
-    return (
-        <ItemDetail/>
-    );
-}
-export default ItemDetailContainer;*/
-
-/*const ItemDetailContainer = () => {
-
-    const[data,setData]=useState({});
-    useEffect(()=>{const getData=  new Promise(resolve=>{setTimeout(()=>{resolve(stockrecomendados);} ,2000);
-}  );
-
-getData.then(res=>setData(res));
-
-})
-  /*const [stockrecomendados, setStockrecomendados] = useState();
 
   useEffect(() => {
-    getProductStock("04").then((stockrecomendados) => {
-      setStockrecomendados();
-    });
-  }, []);
-  const { id } = useParams();
-  console.log(id);
-  if (!stockrecomendados)*/
-    return (
-      <div>
-        Cargando .... espere en breve estará listo !
-        <ItemDetail    id={film.id}
-          nombre={film.nombre}
+    const fetchData = async () => {
+      try {
+        const response = await getFilmsById(id);
+        setFilm(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  
+
+  return (
+    <div className="ItemDetailContainer">
+      <ItemDetail film={film}   
+       titulo={film.titulo}
           genero={film.genero}
           precio={film.precio}
           image ={film.image}
-          categoryId={film.categoryId} />
-        <button>ver detalle</button>
-
-
-
-      </div>
-    );
+          categoryId={film.categoryId}
+          id={film.id} />
+         
+     
+    </div>
+  );
 };
+
 export default ItemDetailContainer;
