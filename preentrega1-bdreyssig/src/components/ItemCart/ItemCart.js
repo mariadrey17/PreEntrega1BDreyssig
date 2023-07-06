1ae538e8f68be3edc9fef12732323c7384bedd1d
mariadrey17 <belydrey@yahoo.com.ar>
2023-07-06 12:26:52 -0300

    agregue itemcart

---
+++ preentrega1-bdreyssig/src/components/ItemCart/ItemCart.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+
+const ItemCart = ({ item }) => {
+  const { removeItem } = useContext(CartContext);
+
+  return (
+    <div className="ItemCart">
+      <img src={item.image} />
+      <div>
+        <p>Titulo: {item.titulo} </p>
+        <p>Cantidad: {item.cantidad} </p>
+        <p>Precio : {item.precio} </p>
+        <p>Total: ${item.cantidad * item.precio} </p>
+        <button onClick={() => removeItem(item.id)}>Eliminar</button>
+      </div>
+    </div>
+  );
+};
+
+export default ItemCart;
